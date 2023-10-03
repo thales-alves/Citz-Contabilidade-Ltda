8959578e4893a7721f6cc37825769b7cfa75c13a
thales-alves <145854863+thales-alves@users.noreply.github.com>
2023-10-02 21:31:17 -0300

    1 e 2 parte

---
+++ script.js
@@ -0,0 +1,62 @@
+function escondido() {
+  var texto = document.getElementById("escondido");
+  if (texto.style.display === "none" || texto.style.display === "") {
+    texto.style.display = "block"; // Exibe o conteúdo quando clicar no texto
+  } else {
+    texto.style.display = "none"; // Oculta o conteúdo se já estiver visível
+  }
+}
+
+function escondido2() {
+  var texto = document.getElementById("escondido2");
+  if (texto.style.display === "none" || texto.style.display === "") {
+    texto.style.display = "block"; // Exibe o conteúdo quando clicar no texto
+  } else {
+    texto.style.display = "none"; // Oculta o conteúdo se já estiver visível
+  }
+}
+
+function escondido3() {
+  var texto = document.getElementById("escondido3");
+  if (texto.style.display === "none" || texto.style.display === "") {
+    texto.style.display = "block"; // Exibe o conteúdo quando clicar no texto
+  } else {
+    texto.style.display = "none"; // Oculta o conteúdo se já estiver visível
+  }
+}
+
+function escondido4() {
+  var texto = document.getElementById("escondido4");
+  if (texto.style.display === "none" || texto.style.display === "") {
+    texto.style.display = "block"; // Exibe o conteúdo quando clicar no texto
+  } else {
+    texto.style.display = "none"; // Oculta o conteúdo se já estiver visível
+  }
+}
+
+function escondido5() {
+  var texto = document.getElementById("escondido5");
+  if (texto.style.display === "none" || texto.style.display === "") {
+    texto.style.display = "block"; // Exibe o conteúdo quando clicar no texto
+  } else {
+    texto.style.display = "none"; // Oculta o conteúdo se já estiver visível
+  }
+}
+
+function escondido6() {
+  var texto = document.getElementById("escondido6");
+  if (texto.style.display === "none" || texto.style.display === "") {
+    texto.style.display = "block"; // Exibe o conteúdo quando clicar no texto
+  } else {
+    texto.style.display = "none"; // Oculta o conteúdo se já estiver visível
+  }
+}
+
+function escondido7() {
+  var texto = document.getElementById("escondido7");
+  if (texto.style.display === "none" || texto.style.display === "") {
+    texto.style.display = "block"; // Exibe o conteúdo quando clicar no texto
+  } else {
+    texto.style.display = "none"; // Oculta o conteúdo se já estiver visível
+  }
+}
