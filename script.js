71bbe0837b4aded3f215200c57818b062caffb24
thales-alves <145854863+thales-alves@users.noreply.github.com>
2023-10-14 20:05:48 -0300

    14/10/2023

--- script.js
+++ script.js
@@ -74,3 +74,83 @@ function checkScroll() {
 // Verifica a posição de rolagem quando a página é carregada e quando a página é rolada
 window.addEventListener('load', checkScroll);
 window.addEventListener('scroll', checkScroll);
+
+function escondidoFAQ1() {
+  var text = document.getElementById("escondidoFAQ1");
+  if (text.style.display === "none" || text.style.display === "") {
+    text.style.display = "block"; // Exibe o conteúdo quando clicar no texto
+  } else {
+    text.style.display = "none"; // Oculta o conteúdo se já estiver visível
+  }
+}
+
+function escondidoFAQ2() {
+  var text = document.getElementById("escondidoFAQ2");
+  if (text.style.display === "none" || text.style.display === "") {
+    text.style.display = "block"; // Exibe o conteúdo quando clicar no texto
+  } else {
+    text.style.display = "none"; // Oculta o conteúdo se já estiver visível
+  }
+}
+
+function escondidoFAQ3() {
+  var text = document.getElementById("escondidoFAQ3");
+  if (text.style.display === "none" || text.style.display === "") {
+    text.style.display = "block"; // Exibe o conteúdo quando clicar no texto
+  } else {
+    text.style.display = "none"; // Oculta o conteúdo se já estiver visível
+  }
+}
+
+function escondidoFAQ4() {
+  var text = document.getElementById("escondidoFAQ4");
+  if (text.style.display === "none" || text.style.display === "") {
+    text.style.display = "block"; // Exibe o conteúdo quando clicar no texto
+  } else {
+    text.style.display = "none"; // Oculta o conteúdo se já estiver visível
+  }
+}
+
+function escondidoFAQ5() {
+  var text = document.getElementById("escondidoFAQ5");
+  if (text.style.display === "none" || text.style.display === "") {
+    text.style.display = "block"; // Exibe o conteúdo quando clicar no texto
+  } else {
+    text.style.display = "none"; // Oculta o conteúdo se já estiver visível
+  }
+}
+
+function escondidoFAQ6() {
+  var text = document.getElementById("escondidoFAQ6");
+  if (text.style.display === "none" || text.style.display === "") {
+    text.style.display = "block"; // Exibe o conteúdo quando clicar no texto
+  } else {
+    text.style.display = "none"; // Oculta o conteúdo se já estiver visível
+  }
+}
+
+
+
+const slides = document.querySelectorAll(".carousel-slide");
+let currentSlide = 0;
+
+function showSlide(slideIndex) {
+    slides[currentSlide].classList.remove("active");
+    slides[slideIndex].classList.add("active");
+    currentSlide = slideIndex;
+}
+
+function nextSlide() {
+    const next = (currentSlide + 1) % slides.length;
+    showSlide(next);
+}
+
+function prevSlide() {
+    const prev = (currentSlide - 1 + slides.length) % slides.length;
+    showSlide(prev);
+}
+
+showSlide(currentSlide);
+setInterval(nextSlide, 5000); // Trocar o slide a cada 5 segundos
+
+
